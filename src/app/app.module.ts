77519e9c69e0f9da1353edf42d57beb797e6d114
Naikx <erik.jutzi@googlemail.com>
2020-09-02 13:20:36 +0200

    changed header. added music, series and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { MoviesComponent } from './movies/movies.component';
+import { SeriesComponent } from './series/series.component';
 import { BooksComponent } from './books/books.component';
+import { MusicComponent } from './music/music.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     MoviesComponent,
-    BooksComponent
+    SeriesComponent,
+    BooksComponent,
+    MusicComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
