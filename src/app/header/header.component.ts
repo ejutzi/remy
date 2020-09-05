054ce5d9992f4d0e52cd0eaeb7efc4fff0f48b51
Naikx <erik.jutzi@googlemail.com>
2020-09-05 16:08:06 +0200

    header reworked. added logo and navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit  {
+  logo = '../../assets/img/dolphin.svg';
 
   constructor() { }
 
