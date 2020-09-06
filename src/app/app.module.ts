705485f3b988a25504bd7b8409f1a114d2816d16
Naikx <erik.jutzi@googlemail.com>
2020-09-06 20:13:27 +0200

    routing added. css for navigation improved.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatExpansionModule } from '@angular/material/expansion';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -14,10 +8,24 @@ import { MoviesComponent } from './movies/movies.component';
 import { SeriesComponent } from './series/series.component';
 import { BooksComponent } from './books/books.component';
 import { MusicComponent } from './music/music.component';
+import { BabyComponent } from './baby/baby.component';
 import { BabyClothingComponent } from './baby/clothing/baby-clothing.component';
 import { BabyEquipmentComponent } from './baby/equipment/baby-equipment.component';
 import { BabyFurnitureComponent } from './baby/furniture/baby-furniture.component';
 import { BabyToysComponent } from './baby/toys/baby-toys.component';
+
+const appRoutes: Routes = [
+  { path: '', component: BabyComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'series', component: SeriesComponent },
+  { path: 'books', component: BooksComponent },
+  { path: 'music', component: MusicComponent },
+  { path: 'baby', component: BabyComponent },
+  { path: 'baby-clothing', component: BabyClothingComponent },
+  { path: 'baby-equipment', component: BabyEquipmentComponent },
+  { path: 'baby-furniture', component: BabyFurnitureComponent },
+  { path: 'baby-toys', component: BabyToysComponent },
+];
 
 @NgModule({
   declarations: [
@@ -27,6 +35,7 @@ import { BabyToysComponent } from './baby/toys/baby-toys.component';
     SeriesComponent,
     BooksComponent,
     MusicComponent,
+    BabyComponent,
     BabyClothingComponent,
     BabyEquipmentComponent,
     BabyFurnitureComponent,
@@ -34,13 +43,7 @@ import { BabyToysComponent } from './baby/toys/baby-toys.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
