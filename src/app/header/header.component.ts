9108cc8670235fbf4ef4f780cbec0591b3f578fd
Naikx <erik.jutzi@googlemail.com>
2020-09-15 04:17:41 +0200

    fix for missing logo.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class HeaderComponent implements OnInit  {
-  logo = './../../assets/img/dolphin.svg'
+  logo = './assets/img/dolphin.svg'
 
   constructor() { }
 
